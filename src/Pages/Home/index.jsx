8dcc7e702fdd03fa1c4c07e526e47a3b28b3c4c0
jmonzon0210 <116613516+jmonzon0210@users.noreply.github.com>
2024-11-20 19:06:50 +0000

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -9,7 +9,7 @@ function Home() {
         </div>
         <div className="w-full md:w-1/2 flex flex-col items-center justify-center text-center m-auto mt-4 md:mt-0">
           <h1 className="text-3xl font-bold">
-            Trastorno del sueño en pacientes pediatricos
+            Trastorno del sueño en pacientes pediátricos
           </h1>
           <p className="text-xl mt-4">
             Esta aplicación utiliza un modelo de inteligencia artificial para
