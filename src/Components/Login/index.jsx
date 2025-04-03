778e2add12a7220611879c03532a392d492fab31
jmonzon0210 <116613516+jmonzon0210@users.noreply.github.com>
2025-04-03 20:52:04 +0200

    Update index.jsx

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -16,7 +16,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        "https://sleepdisorder-detector.duckdns.org/api/login/",
+        "https://sleepdisorder-detector.duckdns.org/api/api/login/",
         { username, password },
         { withCredentials: true } // Envía las cookies
       );
